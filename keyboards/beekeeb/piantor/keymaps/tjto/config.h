99e828996da3f83011eebbcd7e71adb043f76ba5
Tung Bui <tbui@alpha-sense.com>
2025-02-26 00:42:12 +0200

    update tapping term and keymap

--- keyboards/beekeeb/piantor/keymaps/tjto/config.h
+++ keyboards/beekeeb/piantor/keymaps/tjto/config.h
@@ -3,7 +3,7 @@
 #define ACHORDION_STREAK
 
 // Tap-hold configuration for home row mods.
-#define TAPPING_TERM 225
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY
--- tjto.c
+++ tjto.c
@@ -1,4 +1,5 @@
 #include "features/achordion.h"
+#include "keymap_swedish.h"
 
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
   if (!process_achordion(keycode, record)) {
@@ -33,13 +34,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [1] = LAYOUT_split_3x6_3(
         KC_TRNS,  S(KC_1),  S(KC_2),  S(KC_3),  S(KC_4),     S(KC_5),                S(KC_6),     S(KC_7),     S(KC_8),  S(KC_9),  S(KC_0),  S(KC_GRV),
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_LBRC,  S(KC_LBRC),  KC_BSLS,                S(KC_BSLS),  S(KC_RBRC),  KC_RBRC,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+        KC_TRNS,  SE_ADIA,  SE_ARNG,  KC_LBRC,  S(KC_LBRC),  KC_BSLS,                S(KC_BSLS),  S(KC_RBRC),  KC_RBRC,  KC_TRNS,  KC_TRNS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,     KC_TRNS,                KC_TRNS,     KC_TRNS,     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     ),
     [2] = LAYOUT_split_3x6_3(
         KC_TRNS,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_GRV,
-        KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_TRNS,  KC_TRNS,
+        KC_TRNS,  SE_ADIA,  SE_ARNG,  SE_ODIA,  KC_TRNS,  KC_TRNS,                KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_TRNS,  KC_TRNS,
         KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS,           KC_TRNS,  KC_TRNS,  KC_TRNS
     )
