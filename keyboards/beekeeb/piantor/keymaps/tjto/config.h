a5befe067b3ffb5b99f99ddd383819564ced5ed9
Tung Bui <tbui@alpha-sense.com>
2025-02-27 01:27:06 +0200

    chore: unicode and tapping_term tweaks

--- keyboards/beekeeb/piantor/keymaps/tjto/config.h
+++ keyboards/beekeeb/piantor/keymaps/tjto/config.h
@@ -3,10 +3,12 @@
 #define ACHORDION_STREAK
 
 // Tap-hold configuration for home row mods.
-#define TAPPING_TERM 200
+#define TAPPING_TERM 225
 #define TAPPING_TERM_PER_KEY
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM_PER_KEY
 
 // Enable Chordal Hold (https://github.com/qmk/qmk_firmware/pull/24560)
 #define CHORDAL_HOLD
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_LINUX
--- rules.mk
+++ rules.mk
@@ -1,4 +1,5 @@
 COMBO_ENABLE = yes
+UNICODE_ENABLE = yes
 
 OPT_DEFS += -DACHORDION_ENABLE
 SRC += features/achordion.c
